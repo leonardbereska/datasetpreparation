7bc7ce2f301da2f5e56a8c3425c0e9b3fdf4985e
Leonard Bereska <leonard.bereska@hotmail.de>
2018-08-23 13:04:35 +0200

    add_zhang

--- birds.py
+++ birds.py
@@ -40,6 +40,12 @@ def show_img(img):
     plt.show(block=False)
 
 
+def show_kp(img, kp_x, kp_y):
+    plt.imshow(img)
+    plt.scatter(kp_x, kp_y)
+    plt.show(block=False)
+
+
 def print_progress(count, total):
     # Percentage completion.
     pct_complete = float(count) / total
@@ -64,58 +70,6 @@ def wrap_float32(value):
 def wrap_bytes(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
-
-# def convert(data, activ, save_path):
-#     # Args:
-#     # image_paths   List of file-paths for the images.
-#     # labels        Class-labels for the images.
-#     # out_path      File-path for the TFRecords output file.
-#
-#     list_img_path, list_video_nr, list_frame_idx, list_n_frames = data["img_paths"], data["video_idxs"], data["frame_idxs"], data["max_frame_idxs"]
-#
-#     for img_path, video_nr, frame_idx, n_frames in zip(list_img_path, list_video_nr, list_frame_idx, list_n_frames):
-#         # Number of images. Used
-#         # s when printing the progress.
-#         num_images = len(img_path)
-#
-#         # Open a TFRecordWriter for the output-file.
-#
-#         out_path = os.path.join(save_path + activ + "_" + str(video_nr[0]).zfill(2) + ".tfrecords")
-#         print("Converting: " + out_path)
-#         with tf.python_io.TFRecordWriter(out_path) as writer:
-#             # Iterate over all the image-paths and class-labels.
-#             for i, (path, video_nr, frame_idx, n_frames) in enumerate(zip(img_path, video_nr, frame_idx, n_frames)):
-#                 # Print the percentage-progress.
-#                 print_progress(count=i, total=num_images - 1)
-#
-#                 with open(path, 'rb') as f:
-#                     img_raw = f.read()
-#
-#                 # Convert the image to raw bytes.
-#                 # img_bytes = img_raw.tostring()
-#
-#                 # Create a dict with the data we want to save in the
-#                 # TFRecords file. You can add more relevant data here.
-#                 data = \
-#                     {
-#                         'image': wrap_bytes(img_raw),
-#                         'video': wrap_int64(video_nr),
-#                         'frame': wrap_int64(frame_idx),
-#                         'n_frames': wrap_int64(n_frames),
-#                     }
-#
-#                 # Wrap the data as TensorFlow Features.
-#                 feature = tf.train.Features(feature=data)
-#
-#                 # Wrap again as a TensorFlow Example.
-#                 example = tf.train.Example(features=feature)
-#
-#                 # Serialize the data.
-#                 serialized = example.SerializeToString()
-#
-#                 # Write the serialized data to the TFRecords file.
-#
-#                 writer.write(serialized)
 
 def read_table(txt_path):
     with open(txt_path) as file:
@@ -156,7 +110,7 @@ class Birds(object):
     def is_test_set(self, img_idx):
         return bool(self.train_test[img_idx - 1][1])
 
-    def to_tf_records(self, video_name, video_idx, list_imgpaths, list_keypoints, list_max_bbox, save_dir='tfrecords/'):
+    def to_tfrecords(self, video_name, video_idx, list_imgpaths, list_keypoints, list_max_bbox, list_masks, save_dir='tfrecords/'):
         """
         Create tfrecords file from video
         :param video_name: int index of video
@@ -172,7 +126,7 @@ class Birds(object):
         print("Converting: " + out_path)
         with tf.python_io.TFRecordWriter(out_path) as writer:
             # Iterate over all the image-paths and class-labels.
-            for i, (img_path, keypoints, max_bbox) in enumerate(zip(list_imgpaths, list_keypoints, list_max_bbox)):
+            for i, (img_path, keypoints, max_bbox, mask) in enumerate(zip(list_imgpaths, list_keypoints, list_max_bbox, list_masks)):
                 with open(img_path, 'rb') as f:
                     img_raw = f.read()
 
@@ -183,7 +137,8 @@ class Birds(object):
                 data = {'image': wrap_bytes(img_raw),
                         'video': wrap_int64(video_idx),
                         'keypoints': wrap_float32(keypoints),
-                        'bbox_max': wrap_int64(max_bbox)
+                        'bbox_max': wrap_int64(max_bbox),
+                        'masks': wrap_float32(mask),
                         }
 
                 # Wrap the data as TensorFlow Features.
@@ -196,10 +151,10 @@ class Birds(object):
                 serialized = example.SerializeToString()
 
                 # Write the serialized data to the TFRecords file.
-
                 writer.write(serialized)
 
-    def process(self, save_in_dir='processed/', skip_if_video_exists=True, use_mask=True, out_shape=(300, 300, 3), bb_ratio=1.5, only_simple_classes=True, visible_thresh=0.8):
+    def process(self, save_in_dir='processed/', skip_if_video_exists=True, out_shape=(720, 720, 3),
+                only_simple_classes=True, visible_thresh=None, align_parity=True, bb_margin=0.125, exclude_big=True, make_trainset=True):
 
         save_path = self.path + save_in_dir
         make_dir(save_path)
@@ -209,20 +164,17 @@ class Birds(object):
             cat_name = self.classes[cat_idx][1]
             cat_dir = save_path + cat_name
 
-            # Select only good categories:
-            bad_category = False
-            for bad_cat in self.bad_categories:
-                if int(bad_cat[0]) == cat_idx+1 and only_simple_classes:  # is it a bad category?
-                    # if bad_cat[1] == 'fb':  # except flying birds
-                    #     break
-                    # else:
-                    #     continue
-                    print('skip category {}, because {}'.format(cat_name, bad_cat[1]))
-                    bad_category = True
-            if bad_category:
-                continue
+            # exclude certain bird categories (e.g. sea birds, big birds..)
+            if only_simple_classes:  # select only good categories:
+                bad_category = False
+                for bad_cat in self.bad_categories:
+                    if int(bad_cat[0]) == cat_idx+1: # is it a bad category?
+                        print('skip category {}, because {}'.format(cat_name, bad_cat[1]))
+                        bad_category = True
+                if bad_category:
+                    continue
 
-            # make dir
+            # make directory
             if not os.path.exists(cat_dir):
                 os.mkdir(cat_dir)
             else:
@@ -236,27 +188,53 @@ class Birds(object):
             list_kp = []
             list_max_bbox = []
             list_imgpaths = []
+            list_masks = []
+
             for i in images:
                 i -= 1
                 image_path = self.img_path[i][1]
                 image_path = self.path + 'images/' + image_path
                 img = cv2.imread(image_path)
+
+                # train-test split
+                img_in_trainset = bool(int(self.train_test[i][1]))
+                if make_trainset:
+                    if not img_in_trainset:
+                        # print('exclude test set')
+                        continue  # img in test set -> skip for making train set
+                elif not make_trainset:
+                    if img_in_trainset:
+                        # print('exclude train set')
+                        continue  # img in train set -> skip for making test set
+                else:
+                    raise NotImplementedError
+
+                # get bbox
                 bbox = self.bboxes[i][1:5]
-                kp = self.kp[i*15:(i+1)*15]
-                kp_xy = [k[2:5] for k in kp]   # x, y, visible
-
-                # select only good images:
-                kp_visible = [int(k[4]) for k in kp]
-                visible_ratio = np.mean(kp_visible)
-                if visible_ratio < visible_thresh:
-                    print('skip image {}, because kp visible ratio < {}'.format(i, visible_ratio.round(2)))
-                    continue
-
-
                 bbox = [float(bb) for bb in bbox]
                 bbox = [int(bb) for bb in bbox]
 
-                # kp = [float(k) for k in kp
+                # get keypoints
+                kp = self.kp[i*15:(i+1)*15]
+                kp_xy = [k[2:4] for k in kp]   # x, y, visible; dtype in strings
+                kp_x = [kp[0] for kp in kp_xy]
+                kp_y = [kp[1] for kp in kp_xy]
+                kp_x = [float(k) for k in kp_x]  # convert strings to float
+                kp_y = [float(k) for k in kp_y]
+                kp_x = np.array(kp_x)  # convert to array
+                kp_y = np.array(kp_y)
+
+                # create visibility mask for hidden kp
+                kp_visible = [int(k[4]) for k in kp]
+                kp_visible = np.array(kp_visible)
+                kp_visible = kp_visible.astype(np.float32)
+
+                # select only images with visible kp:
+                if visible_thresh is not None:
+                    visible_ratio = np.mean(kp_visible)
+                    if visible_ratio < visible_thresh:
+                        print('skip image {}, because kp visible ratio < {}'.format(i, visible_ratio.round(2)))
+                        continue
 
                 if img is None:
                     print('warning: image does not exist for image {}'.format(i))
@@ -264,105 +242,118 @@ class Birds(object):
                 if bbox is None:
                     print('warning: bbox does not exist for image {}'.format(i))
                     continue
-                bb_x, bb_y, bb_w, bb_h = bbox
-                (h, w, _) = img.shape
-
-
-                # x = kp[0]
-                # y = kp[1]
-
-                c = [int(bb_y+bb_h/2), int(bb_x+bb_w/2)]  # center of bbox
 
                 # exclude images with bbox too close to edges
-                margin = 0.125  # margin to both sides
-                min_bb_ratio = 1. + margin * 2
+                bb_x, bb_y, bb_w, bb_h = bbox
+                max_bbox = np.max([bb_h, bb_w])  # parameter for size of object in image -> later used for zooming
+                (h, w, _) = img.shape
+                c = [int(bb_y + bb_h / 2), int(bb_x + bb_w / 2)]  # center of bbox
+                min_bb_ratio = 1. + bb_margin * 2  # margin to both sides, need complete bird in image
                 too_big, y_min, y_max, x_min, x_max = get_bb(c, min_bb_ratio, bb_w, bb_h, w, h)
-                if too_big:
+                if too_big and exclude_big:
                     print('skip image {}, because bird (bbox) too big in image'.format(i))
                     continue
                 real_bb_ratio = min(w/bb_w, h/bb_h)
                 assert real_bb_ratio >= min_bb_ratio
 
-                # mirror edges to both sides
+                # padding sides: mirror edges to both sides
                 img_length = max(w, h)
 
-                def double_margin(img_, new_margin, c):
+                def double_margin(img_, new_margin, c, kp_x, kp_y):
                     # pad = 1000  # max(new_h, new_w)
                     pad = int((real_bb_ratio-1)/2 * img_length)
                     c[0] += pad
                     c[1] += pad
+                    kp_x += float(pad)
+                    kp_y += float(pad)
                     img_ = np.lib.pad(img_, ((pad, pad), (pad, pad), (0, 0)), 'symmetric')  # pad with a lot (mirror)
                     bb_ratio_crit = 1. + new_margin * 2
                     (new_h, new_w, _) = img_.shape
                     _, y_min, y_max, x_min, x_max = get_bb(c, bb_ratio_crit, bb_w, bb_h, new_w, new_h)  # get box
                     img_ = img_[y_min:y_max, x_min:x_max]  # crop to box
+                    kp_x -= float(x_min)
+                    kp_y -= float(y_min)
                     (new_h, new_w, _) = img_.shape
                     c = [int(new_h/2), int(new_w/2)]  # center of image
-                    return img_, c
+                    return img_, c, kp_x, kp_y
 
-                img, c = double_margin(img, new_margin=0.5, c=c)
-                img, c = double_margin(img, new_margin=1., c=c)
-                # bb_ratio_crit = 1. + 1. * 2
-                # (new_h, new_w, _) = img.shape
-                # new_l = min(new_w, new_h)
-                # _, y_min, y_max, x_min, x_max = get_bb(c, bb_ratio_crit, bb_w, bb_h, new_l, new_l)  # get box
-                # img = img[y_min:y_max, x_min:x_max]  # crop to box
-                # (new_h, new_w, _) = img.shape
-                # c = [int(new_h/2), int(new_w/2)]  # center of image
-                # (new_h, new_w, _) = img.shape
-                # (out_w, out_h, _) = out_shape
-                # _, y_min, y_max, x_min, x_max = get_bb(c, 3., new_w, new_h, out_w, out_h)  # get box
+                img, c, kp_x, kp_y = double_margin(img, new_margin=0.5, c=c, kp_x=kp_x, kp_y=kp_y)
+                img, c, kp_x, kp_y = double_margin(img, new_margin=1., c=c, kp_x=kp_x, kp_y=kp_y)
 
                 # invert image to have same parity (birds looking in one direction)
-                left_eye = kp_visible[6]
-                right_eye = kp_visible[10]
-                if left_eye and right_eye:  # exclude frontal view
-                    print('skip image {}, because both eyes visible'.format(i))
-                    continue  # both visible -> exclude
-                if not left_eye and right_eye:
-                    img = img[:, ::-1, :]  # parity transform if only right eye visible
+                if align_parity:
+                    left_eye = kp_visible[6]
+                    right_eye = kp_visible[10]
+                    if left_eye and right_eye:  # exclude frontal view
+                        print('skip image {}, because both eyes visible'.format(i))
+                        continue  # both visible -> exclude
+                    if not left_eye and right_eye:
+                        img = img[:, ::-1, :]  # parity transform if only right eye visible
+                        _, w, _ = img.shape
+                        kp_x = w - kp_x  # invert kp x location
 
-                img = imresize(img, out_shape)
-                # show_img(img)
+                # resize image to intended final resolution
+                (h_in, w_in, _) = img.shape
+                (h_out, w_out, _) = out_shape
+                img = imresize(img, out_shape)  # resize image
+                kp_x = kp_x / w_in * w_out  # resize kp
+                kp_y = kp_y / h_in * h_out
 
-                # Padding
-                (bb_h, bb_w, _) = img.shape  # height and width of bbox
-                max_bbox = np.max([bb_h, bb_w])  # parameter for size of person in image -> later used for zooming
-
+                kp_x = [kp * kp_visible[k] for k, kp in enumerate(kp_x)]  # mask out hidden kp: set to zero
+                kp_y = [kp * kp_visible[k] for k, kp in enumerate(kp_y)]
 
                 img = img.astype(int)
-                # keypoint_arr = np.concatenate([kp_y, kp_x], axis=0)
-                kp = np.array([0])
+                kp = np.concatenate([kp_y, kp_x], axis=0)
+                kp = kp.astype(np.float32)
 
-                # Save image
+                # save image (for visual debugging)
                 assert(img.shape == out_shape)  # must be rectangular and same size
                 assert (img.dtype == int)  # save as int to reduce disk usage
+                assert kp.dtype == np.float32, print('kp dtype: {}'.format(kp.dtype))
+                assert max(kp_x) <= w_out
+                if max(kp_y) >= h_out:
+                    print('warning: max(kp_y): {} > h_out: {}'.format(max(kp_y), h_out))
+                assert min(kp) >= 0.
+                assert kp_visible.dtype == np.float32
                 frame_path = image_path.replace('images/', save_in_dir)
                 cv2.imwrite(frame_path, img)  # only if well-tested
 
+                # add relevant info to lists
                 list_imgpaths.append(frame_path)
                 list_max_bbox.append(max_bbox)
                 list_kp.append(kp)
+                list_masks.append(kp_visible)
 
             print('video {}'.format(cat_name))
-            self.to_tf_records(cat_name, cat_idx, list_imgpaths, list_kp, list_max_bbox)
+            self.to_tfrecords(cat_name, cat_idx, list_imgpaths, list_kp, list_max_bbox, list_masks)  # save in tfrecords
 
 
-path_to_root = '../../../../myroot/'
-path = path_to_root + 'birds/birds_simple/'
+path_to_root = '/Users/leonardbereska/myroot/'
+path = path_to_root + 'birds/birds/'
 # path_to_harddrive = '/Volumes/Uni/human_backgr/'
-
+assert os.path.exists(path)
 # path = path_to_harddrive + ID + '/'
 
 mybirds = Birds(path_to_dataset=path)
-# build in the following order
-# myhuman.convert_mg4_to_img()  # 1. extracts every nth frame of the mp4
-mybirds.process(save_in_dir='processed/', use_mask=False, out_shape=(720, 720, 3), bb_ratio=3.0, only_simple_classes=True)  # 2. masks out background
+
+mybirds.process(save_in_dir='processed/', out_shape=(720, 720, 3),
+                only_simple_classes=False, visible_thresh=0.0, align_parity=False, bb_margin=0.0, exclude_big=False, make_trainset=True)
 
 
 # Pre-processing Protocol
-# excluded classes: seabirds, flying birds, camouflage and bigger birds
-# mirrored edges (used bbox)
-# exclude frontal view (used eye kp)
-# align parity (used eye kp)  -> most important
-# exclude hidden (used all kp visibility)
+# birds_big
+# - resize to quadratic
+# - center on bbox
+# - excluded classes: seabirds, flying birds, camouflage and bigger birds
+# - mirrored edges (used bbox), excluded too big birds (is this not included in excluding hidden)
+# - exclude frontal view (used eye kp)
+# - align parity (used eye kp)  -> most important step
+# - exclude hidden (used all kp visibility: threshold 0.8)
+
+# bird_parity: like birds_big but without aligning parity and frontal-view exclusion + exclude test_set
+
+# birds_complete
+# - resize
+# - center on bbox, mirror edges multiple times
+# - exclude test set
+
